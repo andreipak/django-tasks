02f2600fb3b5e29733273e8439ab207c8eb49909
Andrei Pak <andrei.pak@gmail.com>
2012-02-07 11:55:09 +0200

    ticket:1 MEDIA_ROOT setting changed

--- testassignment/settings.py
+++ testassignment/settings.py
@@ -53,16 +53,12 @@ USE_L10N = True
 
 # Absolute filesystem path to the directory that will hold user-uploaded files.
 # Example: "/home/media/media.lawrence.com/media/"
-MEDIA_ROOT = ''
+MEDIA_ROOT = os.path.join(PROJECT_ROOT, 'static/')
 
 # URL that handles the media served from MEDIA_ROOT. Make sure to use a
 # trailing slash.
 # Examples: "http://media.lawrence.com/media/", "http://example.com/media/"
 MEDIA_URL = '/static/'
-
-
-if DEBUG:
-    MEDIA_ROOT = os.path.join(PROJECT_ROOT, 'static/')
 
 # Absolute path to the directory static files should be collected to.
 # Don't put anything in this directory yourself; store your static files
