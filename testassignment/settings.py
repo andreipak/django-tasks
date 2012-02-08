3d0b9a20e8ff1c6d875c3430dbe0ed3f971e2923
Andrei Pak <andrei.pak@gmail.com>
2012-02-08 12:24:23 +0200

    ticket:1 added project prefix to MIDDLEWARE_CLASSES

--- testassignment/settings.py
+++ testassignment/settings.py
@@ -111,7 +111,7 @@ MIDDLEWARE_CLASSES = (
     'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
-    'middleware.HttpRequestDbLoggingMiddleware',
+    'testassignment.middleware.HttpRequestDbLoggingMiddleware',
 )
 
 ROOT_URLCONF = 'testassignment.urls'
