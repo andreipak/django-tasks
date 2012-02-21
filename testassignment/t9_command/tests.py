11cce7a3e596b093abefa6b3b5c6944b06031ad7
Andrei Pak <andrei.pak@gmail.com>
2012-02-21 18:20:04 +0200

    ticket:9 listmodels - tests implemented

--- testassignment/t9_command/tests.py
+++ testassignment/t9_command/tests.py
@@ -2,8 +2,15 @@ from django.test import TestCase
 from django.core.management import get_commands, call_command
 
 COMMAND = 'listmodels'
+
 class ListModelsCommandTest(TestCase):
     def test_command_call(self):
         commands_dict = get_commands()
         self.assertTrue(COMMAND in commands_dict)
-        call_command(COMMAND)
+        has_errors = False
+        try:
+            call_command(COMMAND)
+        except:
+            has_errors = True
+
+        self.assertEqual(has_errors, False)
