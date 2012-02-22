22ab8d202c19cdf3e5ac073b2dbb60610c5c02d1
Andrei Pak <andrei.pak@gmail.com>
2012-02-22 11:05:47 +0200

    ticket:10 signals implemented to handle CRUD actions

---
+++ testassignment/t10_signals/admin.py
@@ -0,0 +1,4 @@
+from models import AuditLogEntry
+from django.contrib import admin
+
+admin.site.register(AuditLogEntry)
---
+++ testassignment/t10_signals/signals.py
@@ -0,0 +1,23 @@
+from models import AuditLogEntry
+from datetime import datetime
+
+IGNORELIST = (
+    'AuditLogEntry',
+    'HttpRequestLogEntry',
+    'LogEntry', #admin
+)
+
+def AuditLogger(sender, **kwargs):
+    if sender._meta.object_name in IGNORELIST:
+        return
+
+    action = AuditLogEntry.ACTION_DELETE
+
+    if kwargs.has_key('created'):
+        #created is True if a new record was created https://docs.djangoproject.com/en/dev/ref/signals/#post-save
+        action = kwargs.get('created') and AuditLogEntry.ACTION_CREATE or AuditLogEntry.ACTION_UPDATE
+
+    AuditLogEntry.objects.create(
+        model=sender._meta.object_name,
+        instance=kwargs.get('instance'),
+        action=action)
